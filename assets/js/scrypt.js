a7ad4f3cfcc4d37935fdd9d9f6426109668f273b
Beatriz <lopezcasanovabeatriz@gmail.com>
2020-11-12 01:24:32 -0300

    added the last section

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -1,8 +1,17 @@
-$(function() {
+(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
+
+    $(function() {
+    
+    $(`[data-toggle="tooltip"]`).tooltip()
+    })
    
     
-    $(".card-img-top").click(function(){
-        $(".card-text").toggle("slow", function(){
-        });
-    });
+    $(".card-img-top").click(function () {
+        $(".card-text").toggle("slow", function () {
+    })
+ 
+
 })
+
+
+
