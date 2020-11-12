a59a7db0f06e31340f64e30e45dc75f1a70d2903
Beatriz <lopezcasanovabeatriz@gmail.com>
2020-11-11 21:58:03 -0300

    added js

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -1,15 +1,6 @@
 $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
+   
     
-    $("#enviarCorreo").click(function() {
-        alert("El correo fue enviado correctamente...");
-    });
-
-    $("h5").dblclick(function () {
-        $(this).css(`color`,`red`);
-    });
-
     $(".card-img-top").click(function(){
         $(".card-text").toggle("slow", function(){
         });
